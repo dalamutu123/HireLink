f5d1c033d3f12b325cff29d2778471f1fb780501
David Alamutu <davidalamutu@gmail.com>
2026-05-04 19:52:09 +0100

    chore: setup dotenv, install auth packages, configure PORT from env

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import authRoutes from "./app/auth/auth.routes.js";
 import userRoutes from "./app/users/users.routes.js";
@@ -15,6 +16,7 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
