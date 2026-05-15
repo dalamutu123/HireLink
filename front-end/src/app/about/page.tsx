fe47dfb7b8df4b13ca167142bf315cfc79513a85
Anthony-Okpuruka <anthonyokpuruka999@gmail.com>
2026-05-15 19:42:14 +0100

    Feat: made an about page

---
+++ front-end/src/app/about/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function about() {
+    return (
+        <>
+            <div className="min-h-screen  to-white text-gray-800">
+
+            <h1 className="text-4xl font-bold mb-4">About HireLink</h1>
+            <h2 className="text-gray-500 mb-6">Connect Talent With Opportunity</h2>
+            <p className="text-lg leading-relaxed">
+                HireLink is a modern platform 
+                that helps people discover remote, 
+                hybrid, and on-site jobs from top companies around the world.
+                 Our mission is to make the job search simple, accessible, 
+                 and efficient for everyone.
+            </p>
+          </div>
+        </>
+    )
+}
